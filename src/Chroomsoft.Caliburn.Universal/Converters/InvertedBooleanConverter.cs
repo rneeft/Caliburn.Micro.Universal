ce133ba80e6a939849179d76e068273908d5c4f0
Rick Neeft <rick@chroomsoft.nl>
2017-11-26 19:18:18 +0100

    Added a InvertedBooleanConverter

---
+++ src/Chroomsoft.Caliburn.Universal/Converters/InvertedBooleanConverter.cs
@@ -0,0 +1,7 @@
+﻿namespace Chroomsoft.Caliburn.Universal
+{
+    public class InvertedBooleanConverter : ValueConverterBase<bool, bool>
+    {
+        public override bool Convert(bool value) => !value;
+    }
+}
