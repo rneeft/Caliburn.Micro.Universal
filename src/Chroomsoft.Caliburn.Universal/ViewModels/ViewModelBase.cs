843c79af19ff5e3ed25bc993267c2853ab9d4311
Rick Neeft <rick@chroomsoft.nl>
2018-07-15 20:26:00 +0200

    Added option of catch the property changed event in the ViewModel

--- src/Chroomsoft.Caliburn.Universal/ViewModels/ViewModelBase.cs
+++ src/Chroomsoft.Caliburn.Universal/ViewModels/ViewModelBase.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using Caliburn.Micro;
 
 namespace Chroomsoft.Caliburn.Universal
@@ -24,11 +26,21 @@ namespace Chroomsoft.Caliburn.Universal
 
         protected override void OnActivate()
         {
+            this.PropertyChanged += ViewModelBasePropertyChanged;
             EventAggregator.Subscribe(this);
         }
 
+        private async void ViewModelBasePropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            await OnProperyChangedAsync(e.PropertyName);
+        }
+
+        protected virtual Task OnProperyChangedAsync(string propertyName) { return Task.FromResult(0); }
+
         protected override void OnDeactivate(bool close)
         {
+            this.PropertyChanged -= ViewModelBasePropertyChanged;
+
             EventAggregator.Unsubscribe(this);
         }
 
