3c25aa3eff9a65ee2c7b30820475430c01aba2e9
Rick Neeft <rick@chroomsoft.nl>
2018-07-18 23:16:48 +0200

    Added ObservableList. Added CodeMaid configuration file

---
+++ src/Chroomsoft.Caliburn.Universal/Data/ObservableList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace Chroomsoft.Caliburn.Universal.Data
+{
+    /// <summary> List with observable items that changed as a whole instead of item based. For Item
+    /// based observable list use the <see
+    /// cref="System.Collections.ObjectModel.ObservableCollection<TItem>"/> </summary>
+    public class ObservableList<TItem> : List<TItem>, INotifyCollectionChanged
+    {
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        /// <summary>
+        /// Removed all the items in the list, add the list of items and notify the observers.
+        /// </summary>
+        /// <param name="items">Items to add</param>
+        public void ClearAddRange(IEnumerable<TItem> items)
+        {
+            this.Clear();
+            this.AddRange(items);
+
+            CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+    }
+}
