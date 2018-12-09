96d4b0ece1ef0ca3555e8bd6c931e5317039bfcb
Rick Neeft <rick@chroomsoft.nl>
2018-12-09 14:02:12 +0100

    - Made the container default key public

--- releases.md
+++ releases.md
@@ -1,4 +1,8 @@
 # Releases
+
+## Release 1.6
+- In the container the default `null`, used for the key in the SimpleContainer, is replace by a virtual property `DefaultContainerKey`. Out of the box it still returns `null` but now it can be retrieved and override in your Application.
+
 ## Release 1.5
 - New method in the `ViewModelBase<T>` for updates to the parameter. When navigating with `NavigationProvider.NavigateTo<TViewModel>(parameter)` but TViewModel is already the active ViewModel then the abstract method `OnParameterUpdate(string newValue)` is called. 
 - Example is greatly improved
--- src/Chroomsoft.Caliburn.Universal/Base/BaseContainer.cs
+++ src/Chroomsoft.Caliburn.Universal/Base/BaseContainer.cs
@@ -5,34 +5,43 @@ namespace Chroomsoft.Caliburn.Universal
 {
     public abstract class BaseContainer : WinRTContainer
     {
+        /// <summary>
+        /// Gets the default key for every DI call.
+        /// </summary>
+        public virtual string DefaultContainerKey
+        {
+            get { return null; }
+        }
+
         public void Initialize(IRegisterNavigationFrame frameRegister)
         {
             RegisterWinRTServices();
             this.Singleton<INavigationProvider, NavigationProvider>();
-            RegisterInstance(typeof(IRegisterNavigationFrame), null, frameRegister);
+            RegisterInstance(typeof(IRegisterNavigationFrame), DefaultContainerKey, frameRegister);
         }
 
         public abstract void RegisterApplicationComponents();
+
         public abstract void RegisterOtherViewModels();
 
         public void RegisterShellViewModelAsSingleton(Type shellViewModel)
         {
-            this.RegisterSingleton(shellViewModel, null, shellViewModel);
+            this.RegisterSingleton(shellViewModel, DefaultContainerKey, shellViewModel);
         }
 
         public void RegisterSingleton<TService, TImplementation>() where TService : class where TImplementation : TService
         {
-            this.RegisterSingleton(typeof(TService), null, typeof(TImplementation));
+            this.RegisterSingleton(typeof(TService), DefaultContainerKey, typeof(TImplementation));
         }
 
         public TService GetInstance<TService>() where TService : class
         {
-            return (TService)this.GetInstance(typeof(TService), null);
+            return (TService)this.GetInstance(typeof(TService), DefaultContainerKey);
         }
 
         public void RegisterInstance<TService>(TService service) where TService : class
         {
-            this.RegisterInstance(typeof(TService), null, service);
+            this.RegisterInstance(typeof(TService), DefaultContainerKey, service);
         }
 
         public void RegisterViewModel<TViewModel>() where TViewModel : ViewModelBase
